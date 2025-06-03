359ea2661228e2b89cd6ca2fc2f82cfee82866c9
fugennnnnn <futalanfugen@gmail.com>
2025-06-03 12:48:59 +0800

    3rd activity: Update and Delete

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
-    <!-- Include Bootstrap CSS -->
+  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <!--user icon-->
@@ -62,12 +62,12 @@
                             <td>{{ $std->age }}</td>
                             <td>{{ $std->gender }}</td>
                             <td>
-                                <!-- Edit Button -->
+                             
                                 <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editModal{{ $std->id }}">
                                     Edit
                                 </button>
                     
-                                <!-- Delete Button -->
+                               
                                 <form action="{{ route('students.destroy', $std->id) }}" method="POST" style="display:inline;">
                                     @csrf
                                     @method('DELETE')
