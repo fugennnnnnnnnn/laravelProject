a69afe94615888dcd30737a32258aafffdb8d4ae
fugennnnnn <futalanfugen@gmail.com>
2025-06-14 10:28:48 +0800

    2nd Activity

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -13,6 +13,56 @@
     @yield('content')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<!--
+    
+            <div>
+                <table class="table table-hover">
+                    <thead class="table-dark">
+                        <tr>
+                        
+                            <th scope="col">Student's Name</th>
+                            <th scope="col">Age</th>
+                            <th scope="col">Gender</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @if($students->count() > 0) 
+                      @foreach($students as $std)
+                        <tr>
+                       
+                            <td>{{ $std->name }}</td>
+                            <td>{{ $std->age }}</td>
+                            <td>{{ $std->gender }}</td>
+                            <td>
+                             
+                                <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editModal{{ $std->id }}">
+                                    Edit
+                                </button>
+                    
+                               
+                                <form action="{{ route('students.destroy', $std->id) }}" method="POST" style="display:inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                        @else
+                            <tr>
+                                <td colspan="5" class="text-center">No records found</td>
+                            </tr>
+                        @endif
+                    </tbody>
+                    
+                </table>
+
+
+-->
+
+
 </body>
 
 </html>
